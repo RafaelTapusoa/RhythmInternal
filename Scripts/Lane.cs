05a73c5d7ef078b07985749654844815c28b96ff
RafaelTapusoa <78406787+RafaelTapusoa@users.noreply.github.com>
2022-09-06 10:09:58 +1200

    Add files via upload

---
+++ Scripts/Lane.cs
@@ -0,0 +1,97 @@
+﻿using Melanchall.DryWetMidi.Interaction;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lane : MonoBehaviour
+{
+    // Restricts notes in the MIDI file using DryWetMIDI plugin
+    public Melanchall.DryWetMidi.MusicTheory.NoteName noteRestriction;
+    // Assigns keyboard inputs to notes
+    public KeyCode input;
+    public GameObject notePrefab;
+
+
+    List<Note> notes = new List<Note>();
+    public List<double> timeStamps = new List<double>();
+
+    int spawnIndex = 0;
+    int inputIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetTimeStamps(Melanchall.DryWetMidi.Interaction.Note[] array)
+    {
+        foreach (var note in array)
+        {
+            if (note.NoteName == noteRestriction)
+            {
+                var metricTimeSpan = TimeConverter.ConvertTo<MetricTimeSpan>(note.Time, SongManager.midiFile.GetTempoMap());
+                timeStamps.Add((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds + (double)metricTimeSpan.Milliseconds / 1000f);
+            }
+        }
+    }  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnIndex < timeStamps.Count)
+        {
+            if (SongManager.GetAudioSourceTime() >= timeStamps[spawnIndex] - SongManager.instance.noteTime)
+            {
+                var note = Instantiate(notePrefab, transform);
+                notes.Add(note.GetComponent<Note>());
+                note.GetComponent<Note>().assignedTime = (float)timeStamps[spawnIndex];
+                spawnIndex++;
+            }
+        } 
+
+        // Checking if the user hit the note.
+        if (inputIndex < timeStamps.Count)
+        {
+            double timeStamp = timeStamps[inputIndex];
+            double marginOfError = SongManager.instance.marginOfError;
+            double audioTime = SongManager.GetAudioSourceTime() - (SongManager.instance.inputDelayInMilliseconds / 1000.0);
+
+            // If the input is pressed.
+            if (Input.GetKeyDown(input))
+            {
+                // If the input is pressed within the margin of error then Hit.
+                if (Math.Abs(audioTime - timeStamp) < marginOfError)
+                {
+                    Hit();
+                    print($"Hit on {inputIndex} note");
+                    Destroy(notes[inputIndex].gameObject);
+                    inputIndex++;
+                }
+                else
+                {
+                    print($"Hit inaccurate on {inputIndex} note with {Math.Abs(audioTime - timeStamp)} delay");
+                }
+            }
+
+            // If the key is pressed outside the margin of error then Miss.
+            if (timeStamp + marginOfError <= audioTime)
+            {
+                Miss();
+                print($"Missed {inputIndex} note");
+                inputIndex++;
+            }
+        }    
+    }
+
+    private void Hit()
+    {
+        ScoreManager.Hit();
+    }
+    private void Miss()
+    {
+        ScoreManager.Miss();
+    }
+}
+
